c0f92039d5e0f2a4f5b6703a909775df294fdbd0
Muhun Kim <iam@muhun.kim>
2025-01-03 21:02:47 +0900

    적군 편대 구성에 자주 쓰이는 상수값에 이름 지어서 공유하기

--- app.js
+++ app.js
@@ -8,38 +8,47 @@ function loadTexture(path) {
   })
 }
 
+const MONSTERS_MAX_PER_ROW = 5
+const MONSTERS_COLUMN_GAP = 98
+const MONSTERS_ROW_GAP = 50
+const MONSTER_WIDTH = MONSTERS_MAX_PER_ROW * MONSTERS_COLUMN_GAP
+
 function createEnemies1(ctx, canvas, enemyImg) {
-  const MONSTER_TOTAL = 5
-  const MONSTER_WIDTH = MONSTER_TOTAL * 98
   const START_X = (canvas.width - MONSTER_WIDTH) / 2
   const STOP_X = START_X + MONSTER_WIDTH
 
-  for (let x = START_X; x < STOP_X; x += 98) {
-    for (let y = 0; y < 50 * 5; y += 50) {
+  for (let x = START_X; x < STOP_X; x += MONSTERS_COLUMN_GAP) {
+    for (
+      let y = 0;
+      y < MONSTERS_ROW_GAP * MONSTERS_MAX_PER_ROW;
+      y += MONSTERS_ROW_GAP
+    ) {
       ctx.drawImage(enemyImg, x, y)
     }
   }
 }
 
 function createEnemies2(ctx, canvas, enemyImg) {
-  const MONSTER_TOTAL = 5
-  const MONSTER_WIDTH = MONSTER_TOTAL * 98
   const START_X = (canvas.width - MONSTER_WIDTH) / 2
 
-  for (let row = 0; row < MONSTER_TOTAL; row++) {
-    const numEnemies = MONSTER_TOTAL - row
-    const rowWidth = numEnemies * 98
+  for (let row = 0; row < MONSTERS_MAX_PER_ROW; row++) {
+    const numEnemies = MONSTERS_MAX_PER_ROW - row
+    const rowWidth = numEnemies * MONSTERS_COLUMN_GAP
     const rowStartX = (canvas.width - rowWidth) / 2
 
-    for (let x = rowStartX; x < rowStartX + rowWidth; x += 98) {
-      ctx.drawImage(enemyImg, x, row * 50)
+    for (
+      let x = rowStartX;
+      x < rowStartX + rowWidth;
+      x += MONSTERS_COLUMN_GAP
+    ) {
+      ctx.drawImage(enemyImg, x, row * MONSTERS_ROW_GAP)
     }
   }
 }
 
 window.onload = async () => {
-  canvas = document.getElementById('canvas')
-  ctx = canvas.getContext('2d')
+  const canvas = document.getElementById('canvas')
+  const ctx = canvas.getContext('2d')
   const heroImg = await loadTexture('assets/player.png')
   const enemyImg = await loadTexture('assets/enemyShip.png')
   const starBackground = await loadTexture('assets/starBackground.png')
